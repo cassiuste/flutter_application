2efd0e3c3caced270e2822701a244907ca9201cf
cassius <cassiustedesco1@gmail.com>
2026-03-09 23:09:27 +0100

    Created the createAccountbutton and logginbutton

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,36 +10,35 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login Page", style: TextStyle(color: Colors.white)),
-      backgroundColor: Colors.lightBlue),
-      body: 
-      Center(
-        child: 
-      Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            flutterImage(),
-            const SizedBox(height: 50),
-            
-            userTextBox("Username"),
-            const SizedBox(height: 16),
-
-            passwordTextBox("Password"),
-
-            const SizedBox(height: 20),
-          
-            loginButton(context)
-
-          ],
+      appBar: AppBar(
+        title: const Text("Login Page", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              flutterImage(),
+              const SizedBox(height: 50),
+              userTextBox("Username"),
+              const SizedBox(height: 16),
+              passwordTextBox("Password"),
+              const SizedBox(height: 10),
+              forgotPasswordButton(),
+              const SizedBox(height: 10),
+              loginButton(context),
+            ],
+          ),
         ),
       ),
-    ));
+
+      bottomNavigationBar: createAccountButton(),
+    );
   }
 
-  Widget flutterImage(){
+  Widget flutterImage() {
     return Image.network(
       "https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png",
       height: 80,
@@ -47,38 +46,48 @@ class LoginPage extends StatelessWidget {
   }
 
   Widget userTextBox(String text) {
-  return TextField(
-    controller: _userController,
-    decoration: InputDecoration(
-      labelText: text,
-      border: OutlineInputBorder(),
-    ),
-  );
-}
+    return TextField(
+      controller: _userController,
+      decoration: InputDecoration(
+        labelText: text,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
 
-Widget passwordTextBox(String text) {
-  return TextField(
-    controller: _passwordController,
-    obscureText: true,
-    decoration: InputDecoration(
-      labelText: text,
-      border: OutlineInputBorder(),
-    ),
-  );
-}
+  Widget passwordTextBox(String text) {
+    return TextField(
+      controller: _passwordController,
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: text,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
 
-  Widget loginButton(BuildContext context){
+  Widget forgotPasswordButton() {
+    return TextButton(
+      onPressed: () {},
+      child: const Text(
+        "Forgot password?",
+        style: TextStyle(color: Colors.blue),
+      ),
+    );
+  }
+
+  Widget loginButton(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
         final userValue = _userController.text;
-        final passwordValue = _passwordController.text; 
-        if(userValue == "Usuario" && passwordValue == "12345"){
+        final passwordValue = _passwordController.text;
+        if (userValue == "Usuario" && passwordValue == "12345") {
           Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => CountPage(username: _userController.text),
-              ),
-            );
+            context,
+            MaterialPageRoute(
+              builder: (_) => CountPage(username: _userController.text),
+            ),
+          );
         }
       },
       style: ElevatedButton.styleFrom(
@@ -87,7 +96,25 @@ Widget passwordTextBox(String text) {
         padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
         minimumSize: const Size(200, 50),
       ),
-      child: const Text("Login", style: TextStyle(fontSize: 18)), 
+      child: const Text("Login", style: TextStyle(fontSize: 18)),
+    );
+  }
+
+  Widget createAccountButton() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "New User? Create Account",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
